cc5e45fe079eae1625d997bbef7b147548376dcd
Tan-CM <tanchorming@gmail.com>
2022-03-28 18:09:20 +0800

    Added Concat String practice

---
+++ StringConcate/stringConcate.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var str1 string = "This is the first part"
+	var str2 string = "This is the second part"
+
+	str3 := str1 + " " + str2
+	fmt.Println(str3)
+}
